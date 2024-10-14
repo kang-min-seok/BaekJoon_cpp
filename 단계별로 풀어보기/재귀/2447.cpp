fb7dfa1ba204dc5dfa816f155f93b3760d892833
kang-min-seok <min1042004444@gmail.com>
2024-10-14 15:55:24 +0900

    2024-10-14

--- 단계별로 풀어보기/재귀/2447.cpp
+++ 단계별로 풀어보기/재귀/2447.cpp
@@ -2,18 +2,64 @@
 #include <vector>
 using namespace std;
 
-void star_draw(int a, vector<string> star){
+string expandStar(string star) {
+    string result;
+    for(int i=0;i<3;i++){
+        result += star;
+    }
+    return result;
+}
+
+string expandStar_withBlank(string star){
+    int star_size = star.length()/3;
+    for(int i=star_size; i<star_size+star_size; i++){
+        star[i] = ' ';
+    }
+    // cout << star << '\n';
+    return star;
+}
+
+
+void star_draw(int goal, int now, vector<string> star){
     // 윗부분 출력(공백없이 3번출력)
     // 중앙부분 출력(출력, 공백, 출력)
     // 아랫부분 출력(공백없이 3번 출력)
+    for(int i=0; i<star.size();i++){
+        star[i] = expandStar(star[i]);
+    }
+    int expandSize = star.size();
+    for(int i=0; i<expandSize;i++){
+        star.push_back(expandStar_withBlank(star[i]));
+    }
+    for(int i=0; i<expandSize;i++){
+        star.push_back(star[i]);
+    }
+    if(goal == now){
+        for(int i=0; i<star.size();i++){
+            cout << star[i] << '\n';
+        }
+    } else {
+        now *= 3;
+        star_draw(goal, now, star);
+        return;
+    }
+    return;
 }
 
 
 int main() {
     int num;
-    vector<string>star_field = {"*"};
+    vector<string>star_field = {"***",
+                                "* *",
+                                "***"};
     cin >> num;
-    star_draw(num, star_field);
+    if(num == 3){
+        for(int i=0; i<star_field.size();i++){
+            cout << star_field[i] << "\n";
+        }
+    } else {
+        star_draw(num, 9, star_field);
+    }
 
     return 0;
 }
